894d70fbbd61fc92e3de68efd44960c277fdb322
uNRealCoder <Nikhil_97@hotmail.com>
2021-11-16 00:33:14 -0500

    Update _sliding_window_tfrmer.py

--- Scikit_Extensions_For_Stocks/sliding_window_tranformer/_sliding_window_tfrmer.py
+++ Scikit_Extensions_For_Stocks/sliding_window_tranformer/_sliding_window_tfrmer.py
@@ -1,26 +1,27 @@
 import numpy as np
 import pandas as pd
 from copy import deepcopy
+from sklearn.base import TransformerMixin
 
-def SlidingNWindow(arr,N,col_names=None):
-    if(col_names!=None):
-        assert N==len(col_names), "Column Names array should be equal to size of window"
-    i=0
-    N=20
-    new_arr = []
-    while(i+N<=len(arr)):
-        new_arr.append(arr[i:i+N])
-        i+=1
-    if(col_names==None):
-        return np.array(new_arr)
-    else:
-        DF = pd.DataFrame(np.array(new_arr))
-        DF.columns = col_names
-        return DF
+class SlidingWindowTransformer(TransformerMixin):
+    def __init__(self) -> None:
+        super().__init__()
 
-def SlidingNWindowWithOutput(arr,N,col_names=None):
-    data = SlidingNWindow(arr,N+1,col_names[0:-1])
-    if(col_names==None):
-        return data[:,:-1],data[:,-1]
-    else:
-        return data.iloc[:,:-1],data.iloc[:,-1]
+    def _partialTransform(self,X, y=None,NwindowSize=10,col_names=None ,**fit_params):
+        if(col_names!=None):
+            assert NwindowSize==len(col_names), "Column Names array should be equal to size of window"
+        i=0
+        new_arr = []
+        X = np.array(X)
+        while(i+NwindowSize<=len(X)):
+            new_arr.append(X[i:i+NwindowSize])
+            i+=1
+        if(col_names==None):
+            return np.array(new_arr)
+        else:
+            DF = pd.DataFrame(np.array(new_arr))
+            DF.columns = col_names
+            return DF
+
+    def fit_transform(self,X, y=None,NwindowSize=10,col_names=None ,**fit_params):
+        return self._partialTransform(self,X,y,NwindowSize,col_names, **fit_params)
