24938a25796556ff4040b5d637452a5bb24e26e6
uNRealCoder <Nikhil_97@hotmail.com>
2021-11-16 00:54:32 -0500

    Added check for 1D in SlidingWindow

--- Scikit_Extensions_For_Stocks/sliding_window_tranformer/_sliding_window_tfrmer.py
+++ Scikit_Extensions_For_Stocks/sliding_window_tranformer/_sliding_window_tfrmer.py
@@ -7,12 +7,15 @@ class SlidingWindowTransformer(TransformerMixin):
     def __init__(self) -> None:
         super().__init__()
 
-    def _partialTransform(self,X, y=None,NwindowSize=10,col_names=None ,**fit_params):
+    def _partialTransform(self,X, y=None,NwindowSize=10,col_names=None,force_reshape=False,**fit_params):
         if(col_names!=None):
             assert NwindowSize==len(col_names), "Column Names array should be equal to size of window"
         i=0
         new_arr = []
         X = np.array(X)
+        if(force_reshape==True):
+            X = np.ravel(X)
+        assert X.ndim==1, "Data should be one dimensional, else pass force_reshape=True"
         while(i+NwindowSize<=len(X)):
             new_arr.append(X[i:i+NwindowSize])
             i+=1
@@ -23,5 +26,5 @@ class SlidingWindowTransformer(TransformerMixin):
             DF.columns = col_names
             return DF
 
-    def fit_transform(self,X, y=None,NwindowSize=10,col_names=None ,**fit_params):
-        return self._partialTransform(X,y,NwindowSize,col_names,**fit_params)
+    def fit_transform(self,X, y=None,NwindowSize=10,col_names=None,force_reshape=False,**fit_params):
+        return self._partialTransform(X,y,NwindowSize,col_names,force_reshape,**fit_params)
