5a781a9ad9c3ce5800e8355491f27f61e53f0a18
uNRealCoder <Nikhil_97@hotmail.com>
2021-11-16 01:10:14 -0500

    Implemented FFT
    Hopefully 0  bugs

--- Scikit_Extensions_For_Stocks/fft_transformer/__init__.py
+++ Scikit_Extensions_For_Stocks/fft_transformer/__init__.py
@@ -1,3 +1,3 @@
-from ._fft_1Dtransformer import FFT_Transformer
+from ._fft_1Dtransformer import FFT_1DTransformer,IFFT_1DTransformer
 
-__all__=['FFT_Transformer']
+__all__=['FFT_1DTransformer','IFFT_1DTransformer']
--- Scikit_Extensions_For_Stocks/fft_transformer/_fft_1Dtransformer.py
+++ Scikit_Extensions_For_Stocks/fft_transformer/_fft_1Dtransformer.py
@@ -1,9 +1,23 @@
-import numpy
+import numpy as np
 import pandas
 from sklearn.base import TransformerMixin
-
-class FFT_Transformer(TransformerMixin):
+from numpy import fft
+class FFT_1DTransformer(TransformerMixin):
     def __init__(self) -> None:
         super().__init__()
-    def fit_transform(self, X, y=None, **fit_params):
-        return super().fit_transform(X, y=y, **fit_params)
+    def fit_transform(self, X, y=None, Npoint=None,force_reshape=False ,**fit_params):
+        X = np.array(X)
+        if(force_reshape==True):
+            X = np.ravel(X)
+        assert X.ndim==1, "Data should be one dimensional, else pass force_reshape=True"
+        return fft.fft(X,Npoint,axis=-1)
+
+class IFFT_1DTransformer(TransformerMixin):
+    def __init__(self) -> None:
+        super().__init__()
+    def fit_transform(self, X, y=None, Npoint=None,force_reshape=False ,**fit_params):
+        X = np.array(X)
+        if(force_reshape==True):
+            X = np.ravel(X)
+        assert X.ndim==1, "Data should be one dimensional, else pass force_reshape=True"
+        return fft.ifft(X,Npoint,axis=-1)
